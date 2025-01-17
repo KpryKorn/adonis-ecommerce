4a3a89bb3fc21cf24e729970f902eee88a2c71e7
KpryKorn <roffinisacha@gmail.com>
2025-01-17 12:10:28 +0100

    feat: utilisation des routes "resourceful" d'Adonis

--- start/routes.ts
+++ start/routes.ts
@@ -8,23 +8,10 @@
 */
 
 import router from '@adonisjs/core/services/router'
-
-let renderCount = 0
-let refreshCount1 = 0
-let refreshCount2 = 0
-let lazyCount = 0
-
+const ProduitsController = () => import('#controllers/produits_controller')
 const UsersController = () => import('#controllers/users_controller')
 
 router.on('/').renderInertia('home', { stuff: 'props data' })
 
-router.get('/users', [UsersController, 'index'])
-
-router.get('/login', async (ctx) => {
-  return ctx.inertia.render('auth/login', {
-    renderCount: ++renderCount,
-    refreshCount1: () => ++refreshCount1,
-    refreshCount2: () => ++refreshCount2,
-    lazyCount: ++lazyCount,
-  })
-})
+router.resource('/users', UsersController)
+router.resource('/produits', ProduitsController)
