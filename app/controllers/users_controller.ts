dcf999374c90ba7db5c268e451c130fab50fc56a
KpryKorn <roffinisacha@gmail.com>
2025-01-17 12:09:59 +0100

    feat: ajout du contrôleur Produits et mise à jour du contrôleur Users avec des méthodes CRUD

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,13 +1,43 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class UsersController {
-  index({ inertia }: HttpContext) {
+  /**
+   * Return list of all users or paginate through
+   * them
+   */
+  async index({ inertia }: HttpContext) {
     return inertia.render('users/index', {
-      users: [
-        { id: 1, name: 'Jane Doe' },
-        { id: 2, name: 'John Doe' },
-        { id: 3, name: 'Foo Bar' },
-      ],
+      users: [{ id: 1, name: 'John Doe' }],
     })
   }
+
+  /**
+   * Render the form to create a new user.
+   */
+  async create({}: HttpContext) {}
+
+  /**
+   * Handle form submission to create a new user
+   */
+  async store({ request }: HttpContext) {}
+
+  /**
+   * Display a single user by id.
+   */
+  async show({ params }: HttpContext) {}
+
+  /**
+   * Render the form to edit an existing user by its id.
+   */
+  async edit({ params }: HttpContext) {}
+
+  /**
+   * Handle the form submission to update a specific user by id
+   */
+  async update({ params, request }: HttpContext) {}
+
+  /**
+   * Handle the form submission to delete a specific user by id.
+   */
+  async destroy({ params }: HttpContext) {}
 }
